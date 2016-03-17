0ee31fce7fe9a7f947516832c8c87abda7d400d3
Alen Knejev <knejevalen@gmail.com>
2016-03-17 22:46:24 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+jQuery.extend(jQuery.easing,{
+    easeInOutExpo: function(x, t, b, c, d){
+        if (t==0) return b;
+        if (t==d) return b+c;
+        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+    }
+});
+
+$(document).ready(function(){
+
+    $('a[href="#"]').click(function(e){
+        e.preventDefault();
+        $('html,body').stop().animate({scrollTop: $('.'+$(this).data('scrollto')).offset().top-65 }, 700, 'easeInOutExpo', function(){});
+    });
+    
+    $("header nav ul li a").click(function() {
+            $("header nav ul li a").removeClass("active");
+            $(this).addClass("active");
+        });
+});
+
+

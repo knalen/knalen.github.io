3ba4eac533ea39283fb80bf926246e05accdf1d9
Alen Knejev <kn.alen@yahoo.com>
2016-03-21 09:25:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,6 +18,23 @@ $(document).ready(function(){
             $("header nav ul li a").removeClass("active");
             $(this).addClass("active");
         });
+        
+    $(document).on("scroll", onScroll);
+    
+    function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('header nav ul li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 });
 
 
